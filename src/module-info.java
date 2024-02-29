ca3349551bbe051bc4e05065e77a8ebefb5f5bdf
vettree12 <vettree12@gmail.com>
2024-02-29 19:37:42 +0530

    londonpulled

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vetri
+ *
+ */
+module London {
+}
---
+++ src/org/GitSetup.java
@@ -0,0 +1,9 @@
+package org;
+
+public class GitSetup {
+
+	public static void main(String[] args) {
+		System.out.println("vetri");
+		System.out.println("vetri1");
+	}
+}
